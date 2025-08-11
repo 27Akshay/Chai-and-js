baf9ce01f190ea3ee24ac470d511634a36c3dbff
AKSHAY TALREJA <141359028+27Akshay@users.noreply.github.com>
2025-08-11 16:34:55 +0000

    lec-5 dataypes

--- basics/datatypes5.js
+++ basics/datatypes5.js
@@ -2,7 +2,16 @@
 let one =null;
 let two;
 
-console.log(typeof null);
+// console.log(typeof null);
 // gives object
-console.log(typeof undefined);
+// console.log(typeof undefined);
 //gives undefined 
+
+let three =1;
+let four=""
+let tobool= Boolean(three);
+let tu =Boolean((four));
+console.log(tobool);
+console.log(tu);
+// 1->true 0-> false
+// ""->false "hitesh"->true;
