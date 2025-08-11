e850666e73a52903a62021d477aab087bb7083b7
AKSHAY TALREJA <141359028+27Akshay@users.noreply.github.com>
2025-08-11 15:09:07 +0000

    2nd commit lec-4

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID=12345;
+let accemail="aksh@gmail.com";
+var accname="akshay";
+City="Delhi";
+
+// accountID=34; // not allowed 
+/*do not use var ..bhul jaoo...var me scope issues hote and ...block and functional scope..
+*/ 
+// console.log(accountID);
+console.table([accountID,accemail,accname,City]);
